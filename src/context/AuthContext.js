4437e0f64c4105de50ee44e555f0819903cae02a
Sushant Dhimal <sushant07.dhimal@gmail.com>
2022-10-21 21:41:16 +0545

    add: store user in local storage to prevent web page from reloading

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,14 +1,20 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
 const initialState = {
-  currentUser: null,
+  // currentUser must be object so JSON string must be parsed into JS object
+  currentUser: JSON.parse(localStorage.getItem("user")) || null,
 };
 
 export const AuthContext = createContext(initialState);
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+  useEffect(() => {
+    // need to convert JS object to string to store in localStorage
+    localStorage.setItem("user", JSON.stringify(state.currentUser));
+  }, [state.currentUser]);
 
   return (
     <AuthContext.Provider value={{ currentUser: state.currentUser, dispatch }}>
