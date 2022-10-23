396de836e7668f50e58ffd4fe68a8861e8dd6bda
Sushant Dhimal <sushant07.dhimal@gmail.com>
2022-10-23 22:55:54 +0545

    add: fetch data from cloud firestore

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,12 +1,31 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
-import { userColumns, userRows } from "../../datatablesource";
+import { userColumns } from "../../datatablesource";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
 
 const Datatable = () => {
-  const [data, setData] = useState(userRows);
-
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    console.log("useeffect is running");
+    const fetchDataFromCloudFirestore = async () => {
+      // put asynchronous code inside try/catch block
+      try {
+        let list = [];
+        const querySnapshot = await getDocs(collection(db, "users"));
+        querySnapshot.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+          // console.log(list);
+          setData(list);
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchDataFromCloudFirestore();
+  }, []);
   const handleDelete = (id) => {
     setData(data.filter((item) => item.id !== id));
   };
